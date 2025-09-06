7a7b673b661fc5de6d873a3117d6290fca73bac8
This-Is-Prince <this.is.kumar.prince@gmail.com>
2025-09-06 12:45:30 +0530

    feat: basic array operations

--- array/arrayadt.go
+++ array/arrayadt.go
@@ -1,6 +1,9 @@
 package array
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 type ArrayADT struct {
 	A []int64
@@ -40,14 +43,6 @@ func (a *ArrayADT) FillArray(raw []int64) {
 	a.Append(raw...)
 }
 
-func (a *ArrayADT) Get(index uint) (int64, bool) {
-	if index >= a.Length() {
-		return 0, false
-	}
-
-	return a.A[index], true
-}
-
 func (a *ArrayADT) Insert(index uint, elm int64) {
 	if index > a.Length() {
 		return
@@ -68,6 +63,436 @@ func (a *ArrayADT) Delete(index uint) {
 	}
 
 	a.A = append(a.A[:index], a.A[index+1:]...)
+}
+
+func (a *ArrayADT) LinearSearch(elm int64) int {
+	for idx, v := range a.A {
+		if v == elm {
+			return idx
+		}
+	}
+
+	return -1
+}
+
+func (a *ArrayADT) BinarySearch(elm int64) int {
+	l, h := 0, int(a.Length()-1)
+
+	for l <= h {
+		m := (l + h) / 2
+
+		if a.A[m] == elm {
+			return m
+		} else if a.A[m] > elm {
+			h = m - 1
+		} else {
+			l = m + 1
+		}
+	}
+
+	return -1
+}
+
+func (a *ArrayADT) Get(index uint) (int64, bool) {
+	if index >= a.Length() {
+		return 0, false
+	}
+
+	return a.A[index], true
+}
+
+func (a *ArrayADT) First() (int64, bool) {
+	if a.Length() == 0 {
+		return 0, false
+	}
+
+	return a.A[0], true
+}
+
+func (a *ArrayADT) Last() (int64, bool) {
+	l := a.Length()
+
+	if l == 0 {
+		return 0, false
+	}
+
+	return a.A[l-1], true
+}
+
+func (a *ArrayADT) Set(index uint, value int64) {
+	if index < a.Length() {
+		a.A[index] = value
+	}
+}
+
+func (a *ArrayADT) Max() (int64, bool) {
+	var m int64
+
+	if a.Length() == 0 {
+		return m, false
+	}
+
+	m = a.A[0]
+
+	for _, v := range a.A {
+		if m < v {
+			m = v
+		}
+	}
+
+	return m, true
+}
+
+func (a *ArrayADT) Min() (int64, bool) {
+	var m int64
+
+	if a.Length() == 0 {
+		return m, false
+	}
+
+	m = a.A[0]
+
+	for _, v := range a.A {
+		if m > v {
+			m = v
+		}
+	}
+
+	return m, true
+}
+
+func (a *ArrayADT) Sum() int64 {
+	var s int64
+
+	for _, v := range a.A {
+		s += v
+	}
+
+	return s
+}
+
+func (a *ArrayADT) Avg() float64 {
+	s := a.Sum()
+
+	return float64(s) / float64(a.Length())
+}
+
+func (a *ArrayADT) Reverse() {
+	var i, j uint = 0, a.Length() - 1
+	var tmp int64
+
+	for i < j {
+		tmp = a.A[i]
+		a.A[i] = a.A[j]
+		a.A[j] = tmp
+		i++
+		j--
+	}
+}
+
+func (a *ArrayADT) IsSorted() bool {
+	for i, v := range a.A {
+		if i < int(a.Length())-1 && v > a.A[i+1] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (a *ArrayADT) InsertedInSortedArray(elm int64) bool {
+	if !a.IsSorted() {
+		return false
+	}
+
+	if a.Length() == 0 {
+		a.Append(elm)
+		return true
+	}
+
+	idx := a.Length() - 1
+
+	if a.A[idx] <= elm {
+		a.Append(elm)
+		return true
+	} else {
+		a.Append(a.A[idx])
+	}
+
+	for idx > 0 && a.A[idx-1] > elm {
+		a.A[idx] = a.A[idx-1]
+		idx--
+	}
+
+	a.A[idx] = elm
+	return true
+}
+
+func (a *ArrayADT) NegativeOnLeftSide() {
+	var i, j uint = 0, a.Length() - 1
+	var tmp int64
+
+	for i < j {
+		if a.A[i] < 0 {
+			i++
+		} else if a.A[j] > 0 {
+			j--
+		} else {
+			tmp = a.A[i]
+			a.A[i] = a.A[j]
+			a.A[j] = tmp
+			i++
+			j--
+		}
+	}
+}
+
+func (a *ArrayADT) LeftShift() {
+	if a.Length() == 0 {
+		return
+	}
+
+	i := 0
+
+	for i < int(a.Length())-1 {
+		a.A[i] = a.A[i+1]
+		i++
+	}
+
+	a.A[i] = 0
+}
+
+func (a *ArrayADT) RightShift() {
+	if a.Length() == 0 {
+		return
+	}
+
+	var i = a.Length() - 1
+
+	a.Append(a.A[i])
+
+	for i > 0 {
+		a.A[i] = a.A[i-1]
+		i--
+	}
+
+	a.A[i] = 0
+}
+
+func (a *ArrayADT) Rotate() {
+	if a.Length() <= 1 {
+		return
+	}
+
+	tmp := a.A[0]
+	a.LeftShift()
+	a.A[a.Length()-1] = tmp
+}
+
+func (a *ArrayADT) Merge(b *ArrayADT) *ArrayADT {
+	f := CreateArray(a.Size() + b.Size())
+
+	if a.IsSorted() && b.IsSorted() {
+		var i, j uint = 0, 0
+		for i < a.Length() && j < b.Length() {
+			if a.A[i] <= b.A[j] {
+				f.Append(a.A[i])
+				i++
+			} else if a.A[i] > b.A[j] {
+				f.Append(b.A[j])
+				j++
+			}
+		}
+
+		for i < a.Length() {
+			f.Append(a.A[i])
+			j++
+		}
+
+		for j < b.Length() {
+			f.Append(b.A[j])
+			j++
+		}
+	} else {
+		f.Append(a.A...)
+		f.Append(b.A...)
+	}
+
+	return f
+}
+
+func (a *ArrayADT) Union(b *ArrayADT) *ArrayADT {
+	u := CreateArray(a.Size() + b.Size())
+
+	if a.Length() == 0 {
+		u.Append(b.A...)
+	} else if b.Length() == 0 {
+		u.Append(a.A...)
+	} else if a.IsSorted() && b.IsSorted() {
+		var i, j uint = 0, 0
+
+		if a.A[i] <= b.A[j] {
+			u.Append(a.A[i])
+			i++
+		} else {
+			u.Append(b.A[j])
+			j++
+		}
+
+		for i < a.Length() && j < b.Length() {
+			if a.A[i] <= b.A[j] {
+				if a.A[i] != u.A[u.Length()-1] {
+					u.Append(a.A[i])
+				}
+				i++
+			} else {
+				if b.A[j] != u.A[u.Length()-1] {
+					u.Append(b.A[j])
+				}
+				j++
+			}
+		}
+
+		for i < a.Length() {
+			if a.A[i] != u.A[u.Length()-1] {
+				u.Append(a.A[i])
+			}
+			i++
+		}
+
+		for j < b.Length() {
+			if b.A[j] != u.A[u.Length()-1] {
+				u.Append(b.A[j])
+			}
+			j++
+		}
+	} else {
+		isPresent := false
+
+		for _, v := range a.A {
+			isPresent = false
+			for _, _v := range u.A {
+				if v == _v {
+					isPresent = true
+				}
+			}
+
+			if !isPresent {
+				u.Append(v)
+			}
+		}
+
+		for _, v := range b.A {
+			isPresent = false
+			for _, _v := range u.A {
+				if v == _v {
+					isPresent = true
+				}
+			}
+
+			if !isPresent {
+				u.Append(v)
+			}
+		}
+	}
+
+	return u
+}
+
+func (a *ArrayADT) Intersection(b *ArrayADT) *ArrayADT {
+	u := CreateArray(a.Size() + b.Size())
+
+	if a.Length() == 0 {
+		u.Append(b.A...)
+	} else if b.Length() == 0 {
+		u.Append(a.A...)
+	} else if a.IsSorted() && b.IsSorted() {
+		var i, j uint = 0, 0
+
+		for i < a.Length() && j < b.Length() {
+			if a.A[i] < b.A[j] {
+				i++
+			} else if a.A[i] > b.A[j] {
+				j++
+			} else {
+				elm, ok := u.Last()
+
+				if ok {
+					if elm != a.A[i] {
+						u.Append(a.A[i])
+					}
+				} else {
+					u.Append(a.A[i])
+				}
+
+				i++
+				j++
+			}
+		}
+	} else {
+		for _, v := range a.A {
+			if slices.Contains(b.A, v) {
+				if !slices.Contains(u.A, v) {
+					u.Append(v)
+				}
+			}
+		}
+	}
+
+	return u
+}
+
+func (a *ArrayADT) Difference(b *ArrayADT) *ArrayADT {
+	u := CreateArray(a.Size() + b.Size())
+
+	if a.Length() == 0 {
+		u.Append(b.A...)
+	} else if b.Length() == 0 {
+		u.Append(a.A...)
+	} else if a.IsSorted() && b.IsSorted() {
+		var i, j uint = 0, 0
+
+		for i < a.Length() && j < b.Length() {
+			if a.A[i] < b.A[j] {
+				elm, ok := u.Last()
+
+				if ok {
+					if elm != a.A[i] {
+						u.Append(a.A[i])
+					}
+				} else {
+					u.Append(a.A[i])
+				}
+				i++
+			} else if a.A[i] > b.A[j] {
+				j++
+			} else {
+				i++
+				j++
+			}
+		}
+
+		for i < a.Length() {
+			elm, ok := u.Last()
+
+			if ok {
+				if elm != a.A[i] {
+					u.Append(a.A[i])
+				}
+			} else {
+				u.Append(a.A[i])
+			}
+			i++
+		}
+	} else {
+		for _, v := range a.A {
+			if !slices.Contains(b.A, v) && !slices.Contains(u.A, v) {
+				u.Append(v)
+			}
+		}
+	}
+
+	return u
 }
 
 func RunArrayADT(run bool) {
@@ -97,4 +522,37 @@ func RunArrayADT(run bool) {
 	arr.Delete(0)
 
 	arr.Display()
+
+	fmt.Printf("\n\n")
+
+	fmt.Println("7 is at index:", arr.LinearSearch(7))
+	fmt.Println("1 is at index:", arr.LinearSearch(1))
+	fmt.Println("100 is at index:", arr.LinearSearch(100))
+	fmt.Println("15 is at index:", arr.LinearSearch(15))
+	fmt.Println("7 is at index:", arr.BinarySearch(7))
+	fmt.Println("1 is at index:", arr.BinarySearch(1))
+	fmt.Println("100 is at index:", arr.BinarySearch(100))
+	fmt.Println("15 is at index:", arr.BinarySearch(15))
+
+	fmt.Printf("\n\n")
+
+	elmAt2, ok := arr.Get(2)
+	fmt.Println("Elm at index 2 is:", elmAt2, ok)
+	max, ok := arr.Max()
+	fmt.Println("Max:", max, ok)
+	min, ok := arr.Min()
+	fmt.Println("Min:", min, ok)
+	fmt.Println("Sum:", arr.Sum())
+	fmt.Println("Avg:", arr.Avg())
+	isSorted := arr.IsSorted()
+	fmt.Println("IsSorted:", isSorted)
+	arr.Reverse()
+	arr.Display()
+	isSorted = arr.IsSorted()
+	fmt.Println("IsSorted:", isSorted)
+
+	fmt.Printf("\n\n")
+
+	fmt.Println("Length of Array:", arr.Length())
+	fmt.Println("Size of Array:", arr.Size())
 }
